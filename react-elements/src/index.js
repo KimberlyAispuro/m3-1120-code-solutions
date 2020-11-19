1feed252167ddd91b336128081fe1039c93eefe7
Kimberly Aispuro <kimberlylex@gmail.com>
2020-11-19 08:02:29 +0000

    Completed React Elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = React.createElement(
+    'h1',
+    null,
+    'Hello, React!'
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
