3c8700b64eb797826a941033f1b53283018c1bf1
Kimberly Aispuro <kimberlylex@gmail.com>
2020-12-02 03:40:54 +0000

    Completed react hot button component

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -15,17 +15,17 @@ export default class HotButton extends React.Component{
     render(){
         const countClick = this.state.countClick;
         let buttonClass;
-        if(countClick = 3 && countClick < 6){
+        if(countClick >= 3 && countClick < 6){
             buttonClass = 'purple';
-        } else if(countClick = 6 && countClick < 9) {
+        } else if(countClick >= 6 && countClick < 9) {
             buttonClass = 'lightpurple';
-        } else if(countClick = 9 && countClick < 12){
+        } else if(countClick >= 9 && countClick < 12){
             buttonClass = 'coralpeach';
-        } else if(countClick = 12 && countClick < 15){
+        } else if(countClick >= 12 && countClick < 15){
             buttonClass = 'lightorange';
-        } else if(countClick = 15 && countClick < 18){
+        } else if(countClick >= 15 && countClick < 18){
             buttonClass = 'yellow';
-        } else {
+        } else if(countClick == 18){
             buttonClass = 'white';
         }
 
